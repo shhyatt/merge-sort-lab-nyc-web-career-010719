37094aeec572aeceb76736d1cdcba509cc0038a9
Samantha Hyatt <shhyatt@gmail.com>
2019-07-12 02:15:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function merge(arrayOne, arrayTwo) {
   let sorted = [];
   let currentMin;
   while(arrayOne.length !== 0 && arrayTwo.length !== 0){
-    if(array1[0] < array2[0]){
+    if(arrayOne[0] < arrayTwo[0]){
       sorted.push(findMinAndRemoveSorted(arrayOne))
     }else{
       sorted.push(finsMinAndRemoveSorted(arrayTwo))

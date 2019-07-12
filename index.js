788c3fc584332404c4c965c45f057ba2522ba42b
Samantha Hyatt <shhyatt@gmail.com>
2019-07-12 02:11:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ function merge(array1, array2) {
   while(array1.length != 0 && array2.length != 0){
     if(array1[0] < array2[0]){
       sorted.push(findMinAndRemoveSorted(array1))
+    }else{
+      sorted.push(finsMinAndRemoveSorted(array2))
+    }
       
     }
     

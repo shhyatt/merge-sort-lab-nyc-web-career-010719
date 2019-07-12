19c1d6e5fcb71a957e45bdffd6c2e2e23cb5952d
Samantha Hyatt <shhyatt@gmail.com>
2019-07-12 01:52:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function findMinAndRemoveSorted(array1, array2) {
-  
-  let sorted = [];
-  let min1 = array1.shift(array1[0])
-  let min2 = ift(array2[0])
+
+  let min1 = array1[0]
+  let min2 = array2[0]
   
 }

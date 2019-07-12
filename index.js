1836d3442578b694f16a7b3ba9f55b64beecd8e9
Samantha Hyatt <shhyatt@gmail.com>
2019-07-12 02:11:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function merge(array1, array2) {
   let currentMin;
   while(array1.length != 0 && array2.length != 0){
     if(array1[0] < array2[0]){
-      
+      sorted.push(findMinAndRemoveSorted(array1))
       
     }
     

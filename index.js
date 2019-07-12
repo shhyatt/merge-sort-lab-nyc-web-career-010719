55a9d8f5d89f1a69676664b8f219bc22eb9fa1ef
Samantha Hyatt <shhyatt@gmail.com>
2019-07-12 02:07:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function findMinAndRemoveSorted(array) {
 function merge(array1, array2) {
   let sorted = 
   let currentMin;
-  while(array1.length !== 0 && array2 !==0)
+  while(array1.length != 0 && array2 != 0)
   
   
   }
